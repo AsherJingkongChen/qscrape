8478609a47fc24e47197373c8ba39db18ab886a4
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-02-25 14:57:46 +0800

    (feat): Make explicit bound function iterable and rename the type

--- src/core/Utilities/explicitBind.ts
+++ src/core/Utilities/explicitBind.ts
@@ -9,32 +9,42 @@
  *   + Parameters to bind
  *
  * ## Returns
- * - `ExplicitFunction<TFunction>`
- *   + An explicit bound function which is also an array-like object
+ * - `ExplicitBoundFunction<TFunction>`
+ *   + An explicit bound function with parameters
  */
 export function explicitBind<TFunction extends (...args: any[]) => any>(
   callable: TFunction,
   ...parameters: Parameters<TFunction>
-): ExplicitFunction<TFunction> {
-  return Object.assign(() => callable(...parameters), [
-    callable,
-    ...parameters,
-  ] as const);
+): ExplicitBoundFunction<TFunction> {
+  let result = () => callable(...parameters);
+  const functionAndParameters = [callable, ...parameters] as const;
+  result = Object.assign(result, functionAndParameters);
+  result = Object.assign(result, {
+    [Symbol.iterator]: () => functionAndParameters.values(),
+  });
+  return result as ExplicitBoundFunction<TFunction>;
 }
 
 /**
  * ## Introduction
- * An explicitly bound function
+ * An explicitly bound function.
+ *
+ * It is explicit in terms of the accessibility of the source function and parameters.
  *
  * ## Layout
- * - `()`: `ReturnType<TFunction>`
- *   + Calls the bound function with parameters
- * - `[0]`: `TFunction`
- *   + The source function to bind
- * - `[number]`: `Parameters<TFunction>[number - 1]`
- *   + One of the bound parameters
- *   + `number` is an integer greater than `0`
+ * - `Function`
+ *   - `()`: `ReturnType<TFunction>`
+ *     + Calls the bound function with parameters
+ * - `ArrayLike`
+ *   - `[0]`: `TFunction`
+ *     + The source function to bind
+ *   - `[number]`: `Parameters<TFunction>[number - 1]`
+ *     + One of the bound parameters
+ *     + `number` is an integer greater than `0`
+ * - `Iterable<[TFunction, ...Parameters<TFunction>][number]>`
+ *   + Iterates the source function and parameters
  */
-export type ExplicitFunction<TFunction extends (...args: any[]) => any> =
+export type ExplicitBoundFunction<TFunction extends (...args: any[]) => any> =
   (() => ReturnType<TFunction>) &
-    readonly [TFunction, ...Parameters<TFunction>];
+    readonly [TFunction, ...Parameters<TFunction>] &
+    Iterable<[TFunction, ...Parameters<TFunction>][number]>;
--- tests/core/Utilities.test.ts
+++ tests/core/Utilities.test.ts
@@ -6,7 +6,7 @@ describe('explicitBind()', () => {
     expect(explicitBind(() => {})).toBeTypeOf('function');
   });
 
-  it('It returns an array-like object and the source function is at index 0', () => {
+  it('It returns an array-like object', () => {
     const opt = { index: 4 };
     // NOTE: length is controlled by the bound function
     expect(explicitBind(charAt, 'Hello', opt)).toHaveLength(0);
@@ -15,16 +15,22 @@ describe('explicitBind()', () => {
     expect(explicitBind(charAt, 'Hello', opt)[2]).toBe(opt);
     expect(explicitBind(charAt, 'Hello', opt)[3]).toBeUndefined();
 
-    expect(explicitBind(charAt, 'Hello')).toHaveLength(0);
-    expect(explicitBind(charAt, 'Hello')[0]).toBe(charAt);
-    expect(explicitBind(charAt, 'Hello')[1]).toBe('Hello');
-    expect(explicitBind(charAt, 'Hello')[2]).toBeUndefined();
+    expect(explicitBind(charAt, 'World!')).toHaveLength(0);
+    expect(explicitBind(charAt, 'World!')[0]).toBe(charAt);
+    expect(explicitBind(charAt, 'World!')[1]).toBe('World!');
+    expect(explicitBind(charAt, 'World!')[2]).toBeUndefined();
+  });
+
+  it('It returns an iterable object', () => {
+    const opt = { index: 4 };
+    expect(Array.from(explicitBind(charAt, 'Hello', opt))).toEqual([charAt, 'Hello', opt]);
+    expect(Array.from(explicitBind(charAt, 'the World!'))).toEqual([charAt, 'the World!']);
   });
 
   it('It returns the bound function which returns the correct value', () => {
-    const opt = { index: 4 };
-    expect(explicitBind(charAt, 'Hello', opt)()).toBe('o');
-    expect(explicitBind(charAt, 'Hello')()).toBe('H');
+    const opt = { index: 1 };
+    expect(explicitBind(charAt, 'Ok', opt)()).toBe('k');
+    expect(explicitBind(charAt, 'Ok')()).toBe('O');
     expect(explicitBind(charAt, '')()).toBe('');
   });
 
